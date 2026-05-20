4471af367a83332b86f83c63c8703750560e634f
davidsanbric-dev <davidsanbric@gmail.com>
2026-05-20 11:18:11 -0500

    chore: update environment  values handling in project configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,32 +14,12 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8000/api/v1',
-      firebaseApiKey:
-        process.env.NUXT_PUBLIC_FIREBASE_API_KEY
-        || process.env.FIREBASE_API_KEY
-        || process.env.VITE_FIREBASE_API_KEY
-        || '',
-      firebaseAuthDomain:
-        process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN
-        || process.env.FIREBASE_AUTH_DOMAIN
-        || process.env.VITE_FIREBASE_AUTH_DOMAIN
-        || '',
-      firebaseProjectId:
-        process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID
-        || process.env.FIREBASE_PROJECT_ID
-        || process.env.VITE_FIREBASE_PROJECT_ID
-        || '',
-      firebaseAppId:
-        process.env.NUXT_PUBLIC_FIREBASE_APP_ID
-        || process.env.FIREBASE_APP_ID
-        || process.env.VITE_FIREBASE_APP_ID
-        || '',
-      firebaseMessagingSenderId:
-        process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
-        || process.env.FIREBASE_MESSAGING_SENDER_ID
-        || process.env.VITE_FIREBASE_MESSAGING_SENDER_ID
-        || '',
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || '',
+      firebaseApiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY || '',
+      firebaseAuthDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
+      firebaseProjectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID || '',
+      firebaseAppId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID || '',
+      firebaseMessagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
     },
   },
 })
